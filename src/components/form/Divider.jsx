2cfe6922b5889d404ed2c1cc20ec09d36af18074
Anorme <anormeinkumsah@gmail.com>
2025-08-29 19:19:52 +0000

    feat(auth): modularize UserAuthForm and migrate auth flow from Firebase to API

---
+++ src/components/form/Divider.jsx
@@ -0,0 +1,9 @@
+const Divider = () => (
+  <div className="flex items-center my-4 w-[60%] mx-auto">
+    <hr className="flex-grow border-t-2 border-gray-400" />
+    <span className="mx-4 text-gray-700 font-bold">or</span>
+    <hr className="flex-grow border-t-2 border-gray-400" />
+  </div>
+);
+
+export default Divider;
