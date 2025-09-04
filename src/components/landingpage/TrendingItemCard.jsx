ff6cff7c09d0996e5d6ab134e6e6d64bd341a879
Anorme <anormeinkumsah@gmail.com>
2025-09-04 09:46:42 +0000

    feat(home): scaffold Showcase layout with CategoryList and TrendingCollection. Includes reusable TrendingCard for product display

---
+++ src/components/landingpage/TrendingItemCard.jsx
@@ -0,0 +1,15 @@
+function TrendingItemCard({ name, image, price }) {
+  return (
+    <div className="border-none p-4">
+      <img src={image} alt={name} className="w-[299px] h-[337px] object-cover rounded-none" />
+      <div>
+        <h3 className="mt-3 text-[16px] text-msq-purple font-lato text-left text-nowrap ">
+          {name}
+        </h3>
+        <p className="text-msq-purple-deep text-[20px] font-lato text-left">GHC {price}</p>
+      </div>
+    </div>
+  );
+}
+
+export default TrendingItemCard;
