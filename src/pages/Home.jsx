462851dbc33052719f990cc2f10f3e4337ece581
Anorme <anormeinkumsah@gmail.com>
2025-09-04 09:49:40 +0000

    feat(home): wire components into home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,15 @@
+import CtaSection from '../components/landingpage/CtaSection';
+import HeroSection from '../components/landingpage/HeroSection';
+import Preview from '../components/landingpage/Preview';
+import Showcase from '../components/landingpage/Showcase';
+
 const Home = () => {
   return (
-    <div>
-      <h1>Welcome to MisqabbiGH</h1>
+    <div className="space-y-15 bg-white">
+      <HeroSection />
+      <Showcase />
+      <Preview />
+      <CtaSection />
     </div>
   );
 };
