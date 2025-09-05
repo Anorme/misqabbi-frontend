c416d6d5ba204bf5398b4ffc9d86b2d9c66a72cd
Anorme <anormeinkumsah@gmail.com>
2025-09-05 19:36:21 +0000

    fix(catalogReducer): correct catalog action type imports and change catalogReducer extension from .jsx to .js

--- src/contexts/catalog/catalogReducer.js
+++ src/contexts/catalog/catalogReducer.js
@@ -1,4 +1,4 @@
-import { SET_PRODUCTS, SET_FILTER, SET_PAGE } from './catalogActionTypes';
+import { CATALOG_ACTION_TYPES } from './catalogActionTypes';
 
 export const initialState = {
   products: [], // all products
@@ -9,11 +9,11 @@ export const initialState = {
 
 export const catalogReducer = (state, action) => {
   switch (action.type) {
-    case SET_PRODUCTS:
+    case CATALOG_ACTION_TYPES.SET_PRODUCTS:
       return { ...state, products: action.payload };
-    case SET_FILTER:
+    case CATALOG_ACTION_TYPES.SET_FILTER:
       return { ...state, selectedFilter: action.payload, currentPage: 1 };
-    case SET_PAGE:
+    case CATALOG_ACTION_TYPES.SET_PAGE:
       return { ...state, currentPage: action.payload };
     default:
       return state;
