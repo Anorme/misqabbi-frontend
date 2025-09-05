f236de556e293f3e2c767ec5b2e81a5a6c5d19c0
Anorme <anormeinkumsah@gmail.com>
2025-09-05 19:25:59 +0000

    fix(CatalogProvider): remove unused action imports and scaffold missing dispatch provider

--- src/contexts/catalog/catalogProvider.jsx
+++ src/contexts/catalog/catalogProvider.jsx
@@ -1,20 +1,13 @@
 import React, { useReducer } from 'react';
 import { catalogReducer, initialState } from './catalogReducer';
-import { CatalogContext } from './catalogContext';
-import { setProducts } from './catalogActions,jsx';
-import { setFilter } from './catalogActions,jsx';
-import { setPage } from './catalogActions,jsx';
+import { CatalogContextState, CatalogContextDispatch } from './catalogContext';
 
 export const CatalogProvider = ({ children }) => {
   const [state, dispatch] = useReducer(catalogReducer, initialState);
 
-  const actions = {
-    setProducts: products => dispatch(setProducts(products)),
-    setFilter: filter => dispatch(setFilter(filter)),
-    setPage: page => dispatch(setPage(page)),
-  };
-
   return (
-    <CatalogContext.Provider value={{ state, ...actions }}>{children}</CatalogContext.Provider>
+    <CatalogContextState.Provider value={state}>
+      <CatalogContextDispatch.Provider value={dispatch}>{children}</CatalogContextDispatch.Provider>
+    </CatalogContextState.Provider>
   );
 };
