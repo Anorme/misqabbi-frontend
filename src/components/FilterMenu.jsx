33b65272c31ead86c63e14a0604da72f7199c578
Anorme <anormeinkumsah@gmail.com>
2025-09-09 17:32:09 +0000

    fix(FilterMenu): refactor filter handling to use context state and dispatch

--- src/components/FilterMenu.jsx
+++ src/components/FilterMenu.jsx
@@ -1,6 +1,14 @@
-// src/components/FilterMenu.jsx
+import { useCatalogState, useCatalogDispatch } from '../contexts/catalog/useCatalog';
+import { setFilter } from '../contexts/catalog/catalogActions';
 
-const FilterMenu = ({ options = [], selected, onChange, className = '' }) => {
+const FilterMenu = ({ options = [], className = '' }) => {
+  const { selectedFilter } = useCatalogState();
+  const catalogDispatch = useCatalogDispatch();
+
+  const handleFilterChange = e => {
+    catalogDispatch(setFilter(e.target.value));
+  };
+
   return (
     <div
       className={`flex flex-wrap justify-center gap-2 mt-[20px] lg:mt-[70px] lg:ml-[300px] ${className}`}
@@ -10,8 +18,8 @@ const FilterMenu = ({ options = [], selected, onChange, className = '' }) => {
       </label>
       <select
         id="filter"
-        value={selected}
-        onChange={e => onChange(e.target.value)}
+        value={selectedFilter}
+        onChange={handleFilterChange}
         className="border rounded-lg px-3 py-2 bg-[#c01da8] border-[#c01da8] text-white hover:text-[#c01da8] hover:bg-white focus:outline-none 
         focus:ring-2 focus:ring-[#a78bfa] h-[50px]"
       >
