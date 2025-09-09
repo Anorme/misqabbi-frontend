3fab25f87d4e598f2e5f93033593c9fcb9743ac7
Anorme <anormeinkumsah@gmail.com>
2025-09-09 17:28:28 +0000

    fix(catalog): ensure SET_TOTAL_PAGES action type is defined in CATALOG_ACTION_TYPES

--- src/contexts/catalog/catalogActionTypes.js
+++ src/contexts/catalog/catalogActionTypes.js
@@ -2,4 +2,5 @@ export const CATALOG_ACTION_TYPES = {
   SET_PRODUCTS: 'catalog/SET_PRODUCTS',
   SET_FILTER: 'catalog/SET_FILTER',
   SET_PAGE: 'catalog/SET_PAGE',
+  SET_TOTAL_PAGES: 'catalog/SET_TOTAL_PAGES',
 };
