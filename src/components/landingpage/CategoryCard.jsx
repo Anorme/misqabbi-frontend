fcadbd064a54ddcc55b1cddf070a3f788466e2c6
Nnaedozie Dennis <nnaedozie.dennis@gmail.com>
2025-09-10 16:13:14 +0100

    fix(landing_page:showcase mobile responsiveness)

--- src/components/landingpage/CategoryCard.jsx
+++ src/components/landingpage/CategoryCard.jsx
@@ -1,10 +1,12 @@
 function CategoryCard({ name, image }) {
   return (
-    <div className="flex flex-col items-center mx-2">
-      <div className="w-[164px] h-[164px] rounded-full overflow-hidden border border-none">
+    <div className="flex flex-col items-center justify-center mx-2">
+      <div className="w-[63px] h-[63px] md:w-[164px] md:h-[164px] rounded-full overflow-hidden border border-none">
         <img src={image} alt={name} className="w-full h-full object-cover" />
       </div>
-      <span className="mt-8 text-[24px] font-lato text-msq-purple-rich">{name}</span>
+      <span className="mt-3 md:mt-8 text-[18px] md:text-[24px] font-lato text-msq-purple-rich">
+        {name}
+      </span>
     </div>
   );
 }
