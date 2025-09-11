871677d6485453ef2590742525b2177b32fd700b
Anorme <anormeinkumsah@gmail.com>
2025-09-11 12:16:42 +0000

    fix(router): roll back react router version

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-// import { BrowserRouter as Router } from 'react-router';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router';
 
 import App from './App.jsx';
 import { AuthProvider } from './contexts/auth/AuthProvider.jsx';
