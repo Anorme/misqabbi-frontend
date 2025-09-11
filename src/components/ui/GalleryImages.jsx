5c56338ea6c553f2b0a65654d6c3c1b80671aa79
dermiie <demioyeniyi@gmail.com>
2025-09-11 23:06:53 +0100

    feat: loaded images and modularised image gallery

---
+++ src/components/ui/GalleryImages.jsx
@@ -0,0 +1,21 @@
+function GalleryImages({ product }) {
+  const galleryImages = Array(5).fill(product?.images[0] || product?.images);
+
+  return (
+    <>
+      <ul className="grid grid-cols-5 gap-4 h-28">
+        {galleryImages.map((img, idx) => (
+          <li key={idx}>
+            <img
+              src={img}
+              alt={`${product?.name} thumbnail ${idx + 1}`}
+              className="object-cover h-full w-full rounded"
+            />
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default GalleryImages;
