c518029a2fb22ff72cd8ecbf085f85148fe2d17b
Anorme <anormeinkumsah@gmail.com>
2025-09-11 12:15:05 +0000

    refactor(landingpage): wrap components with SectionWrapper to standardize layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,21 @@ import CtaSection from '../components/landingpage/CtaSection';
 import HeroSection from '../components/landingpage/HeroSection';
 import Preview from '../components/landingpage/Preview';
 import Showcase from '../components/landingpage/Showcase';
+import SectionWrapper from '../components/landingpage/SectionWrapper';
 
 const Home = () => {
   return (
     <div className="space-y-15">
       <HeroSection />
-      <Showcase />
-      <Preview />
-      <CtaSection />
+      <SectionWrapper>
+        <Showcase />
+      </SectionWrapper>
+      <SectionWrapper>
+        <Preview />
+      </SectionWrapper>
+      <SectionWrapper>
+        <CtaSection />
+      </SectionWrapper>
     </div>
   );
 };
