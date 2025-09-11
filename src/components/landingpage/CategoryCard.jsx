ec0999dc305d2475fdc31b635df4aaf9b9ebdc64
Nnaedozie Dennis <nnaedozie.dennis@gmail.com>
2025-09-11 09:59:21 +0100

    fix(landing page): preview mobile responsiveness

--- src/components/landingpage/CategoryCard.jsx
+++ src/components/landingpage/CategoryCard.jsx
@@ -1,7 +1,7 @@
 function CategoryCard({ name, image }) {
   return (
     <div className="flex flex-col items-center justify-center mx-2">
-      <div className="w-[63px] h-[63px] md:w-[164px] md:h-[164px] rounded-full overflow-hidden border border-none">
+      <div className="w-[53px] h-[53px] md:w-[164px] md:h-[164px] rounded-full overflow-hidden border border-none">
         <img src={image} alt={name} className="w-full h-full object-cover" />
       </div>
       <span className="mt-3 md:mt-8 text-[18px] md:text-[24px] font-lato text-msq-purple-rich">
