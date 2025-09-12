653c62166c3e06637a7f47f730462113b413f8f1
Anorme <anormeinkumsah@gmail.com>
2025-09-12 04:32:12 +0000

    layout: switch Navbar and ProductList to fullscreen

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -2,8 +2,8 @@ import { ShoppingCart, Heart, Search } from 'lucide-react';
 
 const NavBar = () => {
   return (
-    <header className="mt-8">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <header className="mt-8 ">
+      <div className="w-full px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           {/* Logo */}
           <div className="flex-shrink-0">
