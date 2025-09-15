f9b8f375502c4a57bbfdee090526ea44b31b34cb
Anorme <anormeinkumsah@gmail.com>
2025-09-15 16:11:42 +0000

    feat(productInfo): update header font color to match designs

--- src/components/ui/ProductInfo.jsx
+++ src/components/ui/ProductInfo.jsx
@@ -1,8 +1,8 @@
 function ProductInfo({ product }) {
   return (
     <div className="flex flex-col gap-2 pb-6">
-      <h1 className="font-extrabold text-2xl tracking-tight text-purple-500">{product?.name}</h1>
-      <p className="text-2xl font-extrabold text-purple-700 hidden lg:block">
+      <h1 className="font-extrabold text-2xl tracking-tight text-[#7D3C98]">{product?.name}</h1>
+      <p className="text-2xl font-extrabold text-msq-purple-deep hidden lg:block">
         GHC {product?.price}
       </p>
       <h2 className="text-2xl">Details</h2>
