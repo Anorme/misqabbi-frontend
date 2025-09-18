e12be3bbede43f02af2eb8ed36aabe8ae317b8c0
Anorme <anormeinkumsah@gmail.com>
2025-09-18 04:03:38 +0000

    fix(cart): flatten subtotal selector to use item price directly

--- src/contexts/cart/cartSelectors.js
+++ src/contexts/cart/cartSelectors.js
@@ -4,4 +4,4 @@ export const getCartItemCount = state =>
   Object.values(state.cartItems).reduce((sum, item) => sum + item.quantity, 0);
 
 export const getCartSubtotal = state =>
-  Object.values(state.cartItems).reduce((sum, item) => sum + item.product.price * item.quantity, 0);
+  Object.values(state.cartItems).reduce((sum, item) => sum + item.price * item.quantity, 0);
