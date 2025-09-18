89668a38d263dd9124ee19b30eb3bc251dda71fc
Anorme <anormeinkumsah@gmail.com>
2025-09-18 02:04:54 +0000

    fix(cart): update selectors to reference cartItems instead of items

--- src/contexts/cart/cartSelectors.js
+++ src/contexts/cart/cartSelectors.js
@@ -1,7 +1,7 @@
-export const getCartItems = state => Object.values(state.items);
+export const getCartItems = state => Object.values(state.cartItems);
 
 export const getCartItemCount = state =>
-  Object.values(state.items).reduce((sum, item) => sum + item.quantity, 0);
+  Object.values(state.cartItems).reduce((sum, item) => sum + item.quantity, 0);
 
 export const getCartSubtotal = state =>
-  Object.values(state.items).reduce((sum, item) => sum + item.product.price * item.quantity, 0);
+  Object.values(state.cartItems).reduce((sum, item) => sum + item.product.price * item.quantity, 0);
