93152638ac116a10e9599979cda2981ab9e50190
Anorme <anormeinkumsah@gmail.com>
2025-09-18 04:57:04 +0000

    refactor(SizeSelect): lift selected size state up to ProductDetails

--- src/components/ui/SizeSelect.jsx
+++ src/components/ui/SizeSelect.jsx
@@ -1,15 +1,8 @@
-import { useState } from 'react';
-
-function SizeSelect({ onChange }) {
+function SizeSelect({ selected, onChange }) {
   const sizes = ['XS', 'S', 'M', 'L', 'XL', 'XXL'];
 
-  const [selectedSize, setSelectedSize] = useState('');
-
   function handleSizeSelect(size) {
-    setSelectedSize(size);
-    if (onChange) {
-      onChange(size);
-    }
+    if (onChange) onChange(size);
   }
 
   return (
@@ -22,7 +15,7 @@ function SizeSelect({ onChange }) {
             onClick={() => handleSizeSelect(size)}
             className={`cursor-pointer border px-4 py-2 rounded-md transition 
               ${
-                selectedSize === size
+                selected === size
                   ? 'bg-[#cfb484] text-white border[#cfb484]'
                   : 'bg-gray-100 border-0 hover:bg-gray-200'
               }`}
