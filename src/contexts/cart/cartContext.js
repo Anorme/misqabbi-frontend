7a5107bb85dc9ca2c917ea309504e111948a7e01
Anorme <anormeinkumsah@gmail.com>
2025-09-18 02:07:18 +0000

    refactor(cart): rename CartContext to CartContextState for clarity

--- src/contexts/cart/cartContext.js
+++ src/contexts/cart/cartContext.js
@@ -1,5 +1,5 @@
 import { createContext } from 'react';
 
-export const CartContext = createContext();
+export const CartContextState = createContext();
 
 export const CartContextDispatch = createContext();
