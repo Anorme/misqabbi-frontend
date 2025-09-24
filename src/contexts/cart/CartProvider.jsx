6febaf4df88dd1ad308632f92c7a4099732a553a
Anorme <anormeinkumsah@gmail.com>
2025-09-24 10:26:00 +0000

    feat(CartProvider): implement cart state hydration and persistence using localStorage

--- src/contexts/cart/CartProvider.jsx
+++ src/contexts/cart/CartProvider.jsx
@@ -1,9 +1,19 @@
-import { useReducer } from 'react';
+import { useReducer, useEffect } from 'react';
 import { cartReducer, initialCartState } from './cartReducer';
 import { CartContextState, CartContextDispatch } from './cartContext';
+import { loadCartState, saveCartState } from '../../utils/cartPersistence';
+
+const getHydratedState = () => {
+  const persistedCartState = loadCartState();
+  return persistedCartState || initialCartState;
+};
 
 export const CartProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(cartReducer, initialCartState);
+  const [state, dispatch] = useReducer(cartReducer, getHydratedState());
+
+  useEffect(() => {
+    saveCartState(state);
+  }, [state]);
 
   return (
     <CartContextState.Provider value={state}>
