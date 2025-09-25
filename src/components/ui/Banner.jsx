c0790428056f5a7ed71e628f10077298f80b56a8
dermiie <demioyeniyi@gmail.com>
2025-09-25 13:55:25 +0100

    style- adjusted banner and footer bg-color to match products standard

--- src/components/ui/Banner.jsx
+++ src/components/ui/Banner.jsx
@@ -1,6 +1,6 @@
 function Banner({ text }) {
   return (
-    <div className="bg-[#81298C] text-sm lg:text-lg font-light md:font-medium tracking-[3px] w-screen h-11 flex items-center text-white font-bebas lg:tracking-wider justify-center">
+    <div className="bg-msq-purple-rich text-sm lg:text-lg font-light md:font-medium tracking-[3px] w-screen h-11 flex items-center text-white font-bebas lg:tracking-wider justify-center">
       {text}
     </div>
   );
