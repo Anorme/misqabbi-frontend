974b9da0a44836ea2b3f3c68ba10464d2bdfde76
dermiie <demioyeniyi@gmail.com>
2025-09-25 13:17:11 +0100

    refactor - display selected navbar based on screen size

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,11 +1,15 @@
+import useMediaQuery from '../../hooks/useMediaQuery.js';
 import Banner from '../ui/Banner.jsx';
 import NavDesktop from '../ui/NavDesktop.jsx';
+import NavMobile from '../ui/NavMobile.jsx';
 
 const NavBar = () => {
+  const isMobile = useMediaQuery('(max-width: 768px)');
+
   return (
     <div>
       <Banner text="SUMMER SALES ONGOING"></Banner>
-      <NavDesktop />
+      {isMobile ? <NavMobile /> : <NavDesktop />}
     </div>
   );
 };
--- src/components/ui/NavMobile.jsx
+++ src/components/ui/NavMobile.jsx
@@ -1,5 +1,26 @@
+import { Bell, Menu, Search, SlidersHorizontal } from 'lucide-react';
+import { Link } from 'react-router';
+
 function NavMobile() {
-  return <div className="max-w-screen-2xl mx-auto px-4 sm:px-6 lg:px-8"></div>;
+  return (
+    <div className="max-w-screen-2xl mx-auto px-4 sm:px-6 lg:px-8">
+      <Menu />
+      <Search />
+      <Link to="/">
+        <div className="flex-shrink-0">
+          <img
+            src="/images/Logo.png"
+            alt="Misqabbi Logo"
+            className="w-28 sm:w-40 md:w-56 object-contain cursor-pointer"
+          />
+        </div>
+      </Link>
+      <SlidersHorizontal />
+      <div className="h-[34px] w-[34px] rounded-full flex justify-center items-center shadow">
+        <Bell />
+      </div>
+    </div>
+  );
 }
 
 export default NavMobile;
