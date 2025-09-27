85e582d18bc2cfed399239e15c9da452d93224e9
Anorme <anormeinkumsah@gmail.com>
2025-09-27 21:06:04 +0000

    refactor(NavDesktop): update NavBar styling to  have fixed positioning

--- src/components/ui/NavDesktop.jsx
+++ src/components/ui/NavDesktop.jsx
@@ -9,8 +9,8 @@ const NavBar = () => {
   const { closeModal, isModalOpen, modalContext } = useAuthAction();
 
   return (
-    <header className="mt-8 ">
-      <div className="w-full px-4 sm:px-6 lg:px-8">
+    <header className="fixed top-0 left-0 right-0 z-50 bg-white shadow-sm">
+      <div className="w-full px-4 sm:px-6 lg:px-8 mt-8">
         <div className="flex items-center justify-between lg:h-16">
           {/* Logo */}
           <NavLogo variant="desktop" />
