af669bc9059b84439b3e985a97600a685d0b7e75
Anorme <anormeinkumsah@gmail.com>
2025-09-27 18:15:57 +0000

    refactor(NavMobile): update layout structure and replace icons for improved navigation

--- src/components/ui/NavMobile.jsx
+++ src/components/ui/NavMobile.jsx
@@ -1,33 +1,36 @@
-import { Bell, Menu, Search, SlidersHorizontal } from 'lucide-react';
+import { ShoppingCart, Heart, Menu } from 'lucide-react';
 import { Link } from 'react-router';
+import SearchBar from './SearchBar.jsx';
 
 function NavMobile() {
   return (
-    <div className="grid grid-cols-[1fr_auto_1fr] items-center max-w-screen-2xl mx-auto px-4 sm:px-6 lg:px-8">
-      {/* Left Section: Menu and Search Icons */}
-      <div className="flex gap-4 items-center py-6">
-        <Menu size={20} className="text-msq-gold-light cursor-pointer" />
+    <div className="max-w-screen-2xl px-4 sm:px-6 lg:px-8">
+      {/* Top Row: Logo and Icons */}
+      <div className="flex justify-between items-center pt-6">
+        {/* Left Section: Logo */}
+        <div className="flex gap-4 items-center">
+          <Link to="/">
+            <div className="flex-shrink-0 h-14">
+              <img
+                src="/images/Logo.png"
+                alt="Misqabbi Logo"
+                className="w-36 sm:w-40 md:w-56 object-contain h-full cursor-pointer"
+              />
+            </div>
+          </Link>
+        </div>
 
-        <Search size={20} className="text-msq-gold-light" />
+        {/* Right Section: Icons */}
+        <div className="flex gap-4 items-center justify-end">
+          <Heart size={20} className="text-msq-gold-light cursor-pointer" />
+          <ShoppingCart size={20} className="text-msq-gold-light cursor-pointer" />
+          <Menu size={20} className="text-msq-gold-light cursor-pointer" />
+        </div>
       </div>
 
-      {/* Center Section: Logo */}
-      <Link to="/">
-        <div className="flex-shrink-0 h-14">
-          <img
-            src="/images/Logo.png"
-            alt="Misqabbi Logo"
-            className="w-36 sm:w-40 md:w-56 object-contain h-full cursor-pointer"
-          />
-        </div>
-      </Link>
-      {/* Right Section: Filter and Notification Icons */}
-      <div className="flex gap-4 items-center py-6 justify-end">
-        <SlidersHorizontal size={20} className="text-msq-gold-light cursor-pointer" />
-
-        <div className="h-[34px] w-[34px] rounded-full flex justify-center p-2 items-center ring-2 ring-neutral-300/80">
-          <Bell size={20} className="text-msq-gold-light cursor-pointer" />
-        </div>
+      {/* Bottom Row: Search Bar */}
+      <div className="">
+        <SearchBar />
       </div>
     </div>
   );
