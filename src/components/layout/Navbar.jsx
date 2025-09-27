63ab33a027991670ad504fbfc5c6d7f2bd2e2fcf
Anorme <anormeinkumsah@gmail.com>
2025-09-27 17:17:00 +0000

    refactor(NavBar): remove Banner component

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,17 +1,12 @@
 import useMediaQuery from '../../hooks/useMediaQuery.js';
-import Banner from '../ui/Banner.jsx';
+
 import NavDesktop from '../ui/NavDesktop.jsx';
 import NavMobile from '../ui/NavMobile.jsx';
 
 const NavBar = () => {
   const isMobile = useMediaQuery('(max-width: 768px)');
 
-  return (
-    <div>
-      <Banner text="SUMMER SALES ONGOING"></Banner>
-      {isMobile ? <NavMobile /> : <NavDesktop />}
-    </div>
-  );
+  return <div>{isMobile ? <NavMobile /> : <NavDesktop />}</div>;
 };
 
 export default NavBar;
