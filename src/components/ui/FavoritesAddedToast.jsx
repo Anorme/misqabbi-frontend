d8e7728c732c97bd84bd35dc70a588ac29e04868
Anorme <anormeinkumsah@gmail.com>
2025-09-27 14:59:03 +0000

    feat(favorites): add FavoritesAddedToast and FavoritesRemovedToast components for user notifications

---
+++ src/components/ui/FavoritesAddedToast.jsx
@@ -0,0 +1,5 @@
+const FavoritesAddedToast = () => {
+  return <div className="text-sm md:text-lg font-medium text-white">Item added to favorites</div>;
+};
+
+export default FavoritesAddedToast;
---
+++ src/components/ui/FavoritesRemovedToast.jsx
@@ -0,0 +1,7 @@
+const FavoritesRemovedToast = () => {
+  return (
+    <div className="text-sm md:text-lg font-medium text-white">Item removed from favorites</div>
+  );
+};
+
+export default FavoritesRemovedToast;
