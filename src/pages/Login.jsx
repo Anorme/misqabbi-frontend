5d63f6a5180829f2c242d4687ebbb2b0f2d21b57
dermiie <demioyeniyi@gmail.com>
2025-10-02 01:10:39 +0100

    style(login): quick fix to image height

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -10,12 +10,7 @@ const Login = () => {
       <div className="flex bg-white rounded-2xl overflow-hidden lg:shadow-lg w-full max-w-4xl h-[90%]">
         {/* Image section */}
         <div className="w-[45%] hidden md:flex items-stretch">
-          <img
-            src={loginImg}
-            alt="Visual"
-            className="object-cover w-full h-full"
-            style={{ height: '714px' }}
-          />
+          <img src={loginImg} alt="Visual" className="object-cover w-full h-full" />
         </div>
         <div className="w-full md:w-[55%] p-10 flex flex-col justify-center pt-8 pb-8">
           <h2 className="font-bebas text-[25px] font-bold uppercase text-purple-700 text-center mb-6">
