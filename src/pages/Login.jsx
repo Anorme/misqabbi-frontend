d88e5de688173b3a79e606c4537b5d1e1d304480
Anorme <anormeinkumsah@gmail.com>
2025-10-06 11:25:23 +0000

    feat(login): enhance layout and responsiveness of login page with media query

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,29 +3,29 @@ import { Link } from 'react-router';
 import UserAuthForm from '../components/ui/UserAuthForm';
 
 import loginImg from '../assets/login.png';
+import useMediaQuery from '../hooks/useMediaQuery';
 
 const Login = () => {
+  const isMdUp = useMediaQuery('(min-width: 768px)');
+
   return (
-    <div className="min-h-screen w-screen flex items-center justify-center bg-purple-300 p-0 m-0 font-lato">
-      <div
-        className="flex bg-white rounded-2xl overflow-hidden shadow-lg mt-12 mb-12"
-        style={{ height: '714px', width: '1000px' }}
-      >
-        <div className="w-[45%] hidden md:flex items-stretch">
-          <img
-            src={loginImg}
-            alt="Visual"
-            className="object-cover w-full h-full"
-            style={{ height: '714px' }}
-          />
-        </div>
-        <div className="w-full md:w-[55%] p-10 flex flex-col justify-center pt-8 pb-8">
-          <h2 className="font-bebas text-[25px] font-bold uppercase text-purple-700 text-center mb-6">
-            LOGIN
+    <div className="min-h-screen w-screen flex md:items-center justify-center bg-white p-0 m-0 font-lato">
+      <div className="w-full max-w-md sm:max-w-lg md:max-w-4xl lg:max-w-5xl flex bg-white rounded-none md:rounded-2xl overflow-visible md:overflow-hidden md:shadow-lg mx-0 md:mx-4 my-0 md:my-16 min-h-screen md:min-h-0">
+        {isMdUp && (
+          <div className="w-[45%] hidden md:flex items-stretch">
+            <img src={loginImg} alt="Visual" className="object-cover w-full h-full" />
+          </div>
+        )}
+
+        <div className="w-full md:w-[55%] p-6 sm:p-8 flex flex-col justify-center">
+          <h2 className="font-bebas text-[22px] sm:text-[24px] font-bold uppercase text-msq-purple-rich text-center mb-[74px] sm:mb-6">
+            Log in
           </h2>
+
           <UserAuthForm mode="login" />
-          <Link to="/register" className="text-purple-700 font-medium mt-4 text-center">
-            <span className="font-corsiva">Create Account?</span>
+
+          <Link to="/register" className="text-msq-purple-rich font-medium mt-6 text-center">
+            <span className="font-corsiva">Create account?</span>
           </Link>
         </div>
       </div>
