760f2fee4d269403c32eb0c7124a5e257b53619e
Anorme <anormeinkumsah@gmail.com>
2025-10-06 13:15:29 +0000

    fix(HeroMobile): update image width class for improved responsiveness on mobile devices

--- src/components/landingpage/HeroMobile.jsx
+++ src/components/landingpage/HeroMobile.jsx
@@ -5,7 +5,7 @@ const HeroMobile = () => (
     <img
       src="https://images.unsplash.com/photo-1546536133-d1b07a9c768e?q=80&w=986&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
       alt="Hero Image"
-      className="w-[409px] max-w-full h-[216px] mx-auto object-cover"
+      className="w-full max-w-full h-[216px] mx-auto object-cover"
     />
 
     <div className=" px-6 py-8 text-center">
