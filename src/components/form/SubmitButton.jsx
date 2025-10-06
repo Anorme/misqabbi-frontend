fbbd395dddfdddbbbcedfe84a8c23a656ec602a7
Anorme <anormeinkumsah@gmail.com>
2025-10-06 11:27:59 +0000

    feat(ui): update SubmitButton styles for improved responsiveness and visual consistency

--- src/components/form/SubmitButton.jsx
+++ src/components/form/SubmitButton.jsx
@@ -1,8 +1,8 @@
 const SubmitButton = ({ label, disabled }) => (
   <button
     type="submit"
-    className={`w-[60%] mx-auto bg-purple-700 text-white py-3 font-semibold ${
-      !disabled ? 'cursor-pointer hover:bg-purple-500' : 'opacity-50 cursor-not-allowed'
+    className={`w-full sm:w-[60%] mx-auto bg-msq-purple-rich text-white py-3 font-semibold ${
+      !disabled ? 'cursor-pointer hover:bg-msq-purple-light' : 'opacity-50 cursor-not-allowed'
     }`}
     style={{ borderRadius: '18px' }}
     disabled={disabled}
