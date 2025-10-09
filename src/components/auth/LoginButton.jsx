da6be6f46015708af4662fce472b6e8620283e27
Anorme <anormeinkumsah@gmail.com>
2025-10-09 10:49:48 +0000

    feat(AuthButtons): enhance components with className and onClick props for better customization

--- src/components/auth/LoginButton.jsx
+++ src/components/auth/LoginButton.jsx
@@ -1,8 +1,11 @@
 import { Link } from 'react-router';
 
-const LoginButton = () => (
-  <Link to="/login">
-    <button className="text-[#d265ff] hover:text-msq-purple-rich font-medium cursor-pointer">
+const LoginButton = ({ className = '', onClick, ...props }) => (
+  <Link to="/login" onClick={onClick}>
+    <button
+      className={`text-msq-purple-light hover:text-msq-purple-rich font-medium cursor-pointer ${className}`}
+      {...props}
+    >
       Login
     </button>
   </Link>
