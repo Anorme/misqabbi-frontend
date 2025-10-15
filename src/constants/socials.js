7a50595d5e5ddb8a3b128ae428c5552ca819a284
Anorme <anormeinkumsah@gmail.com>
2025-10-15 14:32:29 +0000

    feat(Socials): add SOCIALS_MAP constant for social media links

---
+++ src/constants/socials.js
@@ -0,0 +1,6 @@
+export const SOCIALS_MAP = {
+  Instagram: 'https://www.instagram.com/misqabbigh',
+  Tiktok: 'https://www.tiktok.com/@misqabbigh',
+  WhatsApp: 'https://wa.me/233507226511',
+  Email: 'mailto:info@misqabbigh.com',
+};
