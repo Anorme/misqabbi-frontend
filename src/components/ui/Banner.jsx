3d2e1982e6d7d1dff99fc9e6a08725d28a849c4b
Anorme <anormeinkumsah@gmail.com>
2025-10-18 22:07:40 +0000

    chore(ui): remove unused Banner, Button, Card, and ColorPalette components to clean up the codebase

--- src/components/ui/Banner.jsx
+++
@@ -1,9 +0,0 @@
-function Banner({ text }) {
-  return (
-    <div className="bg-msq-purple-rich text-sm lg:text-lg font-light md:font-medium tracking-[3px] w-screen h-11 flex items-center text-white font-bebas lg:tracking-wider justify-center">
-      {text}
-    </div>
-  );
-}
-
-export default Banner;
