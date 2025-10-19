3f6f0de28c17732312c5728c0a621cded3ab81ba
Anorme <anormeinkumsah@gmail.com>
2025-10-19 11:36:59 +0000

    fix: add getCartItemsWithKeys selector to preserve unique keys from cart reducer

--- src/contexts/cart/cartSelectors.js
+++ src/contexts/cart/cartSelectors.js
@@ -1,5 +1,8 @@
 export const getCartItems = state => Object.values(state.cartItems);
 
+export const getCartItemsWithKeys = state =>
+  Object.entries(state.cartItems).map(([key, item]) => ({ ...item, key }));
+
 export const getCartItemCount = state =>
   Object.values(state.cartItems).reduce((sum, item) => sum + item.quantity, 0);
 
