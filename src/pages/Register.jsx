686b0b6858d22fd2b3275a15316f13c21a67bbbe
Anorme <anormeinkumsah@gmail.com>
2025-10-31 13:54:15 +0000

    feat(Login, Register): integrate SEO component for enhanced page metadata and indexing control

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -2,12 +2,18 @@ import UserAuthForm from '../components/auth/UserAuthForm';
 import signupImg from '../assets/signup.png';
 import { Link } from 'react-router';
 import useMediaQuery from '../hooks/useMediaQuery';
+import SEO from '../components/SEO';
 
 const Register = () => {
   const isMdUp = useMediaQuery('(min-width: 768px)');
 
   return (
     <div className="min-h-screen w-screen flex md:items-center justify-center bg-white p-0 m-0 font-lato">
+      <SEO
+        title="Register"
+        description="Create your Misqabbi account to start shopping premium fashion tailored for you."
+        robots="noindex,nofollow"
+      />
       <div className="w-full max-w-md sm:max-w-lg md:max-w-4xl lg:max-w-5xl flex bg-white rounded-none md:rounded-2xl overflow-visible md:overflow-hidden md:shadow-lg mx-0 md:mx-4 my-0 md:my-16 min-h-screen md:min-h-0">
         {isMdUp && (
           <div className="w-[45%] hidden md:flex items-stretch">
