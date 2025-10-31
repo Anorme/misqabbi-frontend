bc7067325117993fcc05ef306bf925aa6ca6cddc
Anorme <anormeinkumsah@gmail.com>
2025-10-31 13:04:40 +0000

    feat(Home): integrate SEO component for improved page metadata and description

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,10 +3,17 @@ import HeroSection from '../components/landingpage/HeroSection';
 import Preview from '../components/landingpage/Preview';
 import Showcase from '../components/landingpage/Showcase';
 import SectionWrapper from '../components/landingpage/SectionWrapper';
+import SEO from '../components/SEO';
 
 const Home = () => {
   return (
     <div className="space-y-15">
+      <SEO
+        title="Uniquely Made for Her"
+        description="Discover Misqabbi, a women's wear brand that's all about the girlies. Explore curated lounge and casual pieces made to empower her. Soft, elegant and uniquely crafted for her."
+        canonicalPath="/"
+        titleTemplate="Misqabbi | {title}"
+      />
       <HeroSection />
       <SectionWrapper>
         <Showcase />
