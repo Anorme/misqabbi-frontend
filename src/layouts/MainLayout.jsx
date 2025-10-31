283245228d57e7837aa9f2bfa250e66a95143e39
Anorme <anormeinkumsah@gmail.com>
2025-10-31 13:39:14 +0000

    refactor(MainLayout): remove SEO component to streamline layout structure

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,16 +1,9 @@
 import NavBar from '../components/layout/Navbar';
 import Footer from '../components/layout/Footer';
-import SEO from '../components/SEO';
 
 const MainLayout = ({ children }) => {
   return (
     <div className="flex flex-col min-h-screen justify-between">
-      <SEO
-        title="Uniquely Made for Her"
-        description="Discover Misqabbi, a women's wear brand that's all about the girlies. Explore curated lounge and casual pieces made to empower her. Soft, elegant and uniquely crafted for her."
-        canonicalPath="/"
-        titleTemplate="Misqabbi | {title}"
-      />
       <NavBar />
       <div className="pt-24">{children}</div>
       <Footer />
