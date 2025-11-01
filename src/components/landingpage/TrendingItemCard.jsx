4eee27c95cec076920e06599a4e2e5f563ce4c66
Anorme <anormeinkumsah@gmail.com>
2025-11-01 03:18:26 +0000

    style(TrendingItemCard): adjust image height to full for better responsiveness

--- src/components/landingpage/TrendingItemCard.jsx
+++ src/components/landingpage/TrendingItemCard.jsx
@@ -1,11 +1,7 @@
 function TrendingItemCard({ name, image, price }) {
   return (
     <div className="border-none">
-      <img
-        src={image}
-        alt={name}
-        className="w-full h-[205px] md:h-[337px] object-cover rounded-none"
-      />
+      <img src={image} alt={name} className="w-full h-full object-cover rounded-none" />
       <div>
         <h3 className="mt-3 text-[16px] text-msq-purple font-lato text-left text-nowrap ">
           {name}
