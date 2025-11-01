926a89a10eb0ed8c8319203e7341e60d86c5f78b
Anorme <anormeinkumsah@gmail.com>
2025-11-01 10:58:06 +0000

    style(TrendingItemCard): adjust text sizes for better responsiveness and update image height for improved layout

--- src/components/landingpage/TrendingItemCard.jsx
+++ src/components/landingpage/TrendingItemCard.jsx
@@ -1,12 +1,14 @@
 function TrendingItemCard({ name, image, price }) {
   return (
     <div className="border-none">
-      <img src={image} alt={name} className="w-full h-full object-cover rounded-none" />
+      <img src={image} alt={name} className="w-full h-[3/4] object-cover rounded-none" />
       <div>
-        <h3 className="mt-3 text-[16px] text-msq-purple font-lato text-left text-nowrap ">
+        <h3 className="mt-3 text-[14px] md:text-[16px] lg:text-[18px] text-msq-purple font-lato text-left text-nowrap ">
           {name}
         </h3>
-        <p className="text-msq-purple-deep text-[20px] font-lato text-left">GHC {price}</p>
+        <p className="text-msq-purple-deep text-[14px] md:text-[18px] lg:text-[20px] font-lato text-left">
+          GHC {price}
+        </p>
       </div>
     </div>
   );
