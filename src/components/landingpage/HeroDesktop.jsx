ed21cf1314549236913ab677163418e650db3811
Anorme <anormeinkumsah@gmail.com>
2025-11-02 18:28:03 +0000

    refactor(HeroDesktop): replace background image with img tag for better loading performance and accessibility

--- src/components/landingpage/HeroDesktop.jsx
+++ src/components/landingpage/HeroDesktop.jsx
@@ -1,15 +1,17 @@
 import { Link } from 'react-router';
 
 const HeroDesktop = () => (
-  <section
-    className="w-full h-screen max-w-screen-6xl mx-auto bg-center bg-cover"
-    style={{
-      backgroundImage:
-        "url('https://res.cloudinary.com/dyciw970t/image/upload/f_auto,q_auto,w_1920,c_limit/v1761952396/misqabbi/products/CSI_0053_sqkfoq.jpg')",
-    }}
-  >
+  <section className="relative w-full h-screen max-w-screen-6xl mx-auto overflow-hidden">
+    {/* Hero image */}
+    <img
+      src="https://res.cloudinary.com/dyciw970t/image/upload/f_auto,q_auto,w_1920,c_limit/v1761952396/misqabbi/products/CSI_0053_sqkfoq.jpg"
+      alt="Hero Image"
+      className="absolute inset-0 w-full h-full object-cover object-center"
+      fetchPriority="high"
+      loading="eager"
+    />
     {/* Content container */}
-    <div className=" z-10 h-full px-8 flex items-center justify-center">
+    <div className="relative z-10 h-full px-8 flex items-center justify-center">
       <div className="max-w-[1050px]">
         <h1 className="text-[64px] font-bebas text-white leading-tight text-nowrap">
           STEP INTO TIMELESS ELEGANCE.
