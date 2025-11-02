536ca4c3e3b5c3d8f0bcca91f2051268a1a1e718
Anorme <anormeinkumsah@gmail.com>
2025-11-02 14:17:03 +0000

    refactor(hooks): move useProduct hook into useProducts file and remove standalone file

--- src/hooks/queries/useProducts.js
+++ src/hooks/queries/useProducts.js
@@ -1,5 +1,5 @@
 import { useQuery } from '@tanstack/react-query';
-import { fetchDiscoverableProducts } from '../../api/products';
+import { fetchDiscoverableProducts, fetchProductBySlug } from '../../api/products';
 
 /**
  * Query hook for fetching discoverable products with filters
@@ -30,6 +30,22 @@ export const useProducts = (params = {}, options = {}) => {
       fetchDiscoverableProducts({ page, limit, q, category, minPrice, maxPrice, sort }),
     staleTime: 10 * 60 * 1000, // 10 minutes
     gcTime: 30 * 60 * 1000, // 30 minutes
+    ...options,
+  });
+};
+
+/**
+ * Query hook for fetching a single product by slug
+ * @param {string} slug - Product slug
+ * @param {Object} options - Additional query options
+ */
+export const useProduct = (slug, options = {}) => {
+  return useQuery({
+    queryKey: ['products', 'slug', slug],
+    queryFn: () => fetchProductBySlug(slug),
+    enabled: !!slug, // Only fetch if slug is provided
+    staleTime: 10 * 60 * 1000, // 10 minutes
+    gcTime: 60 * 60 * 1000, // 1 hour
     ...options,
   });
 };
