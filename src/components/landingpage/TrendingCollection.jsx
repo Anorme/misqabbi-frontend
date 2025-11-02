d345a635c58b13a29c480907e3622bbe85899364
Anorme <anormeinkumsah@gmail.com>
2025-11-02 19:55:04 +0000

    refactor(TrendingCollection): update item names and adjust prices for improved accuracy

--- src/components/landingpage/TrendingCollection.jsx
+++ src/components/landingpage/TrendingCollection.jsx
@@ -5,25 +5,25 @@ const items = [
     name: 'Ileme Dress',
     image:
       'https://res.cloudinary.com/dyciw970t/image/upload/f_auto,q_auto,w_600,c_limit/v1761965676/misqabbi/landing-page/CSI_0041_n1o6cm.jpg',
-    price: 300,
+    price: 210,
   },
   {
-    name: 'Missy J Boubou',
+    name: 'MJ Boubou',
     image:
       'https://res.cloudinary.com/dyciw970t/image/upload/f_auto,q_auto,w_600,c_limit/v1761965808/misqabbi/landing-page/CSI_0093_ila3wb.jpg',
-    price: 300,
+    price: 270,
   },
   {
     name: 'Mayah Skirt',
     image:
       'https://res.cloudinary.com/dyciw970t/image/upload/f_auto,q_auto,w_600,c_limit/v1761965680/misqabbi/landing-page/CSI_9944_dsvzfv.jpg',
-    price: 300,
+    price: 200,
   },
   {
     name: 'Belle Pants',
     image:
       'https://res.cloudinary.com/dyciw970t/image/upload/f_auto,q_auto,w_600,c_limit/v1761957952/misqabbi/landing-page/CSI_9971_yqxnkp.jpg',
-    price: 300,
+    price: 290,
   },
 ];
 
