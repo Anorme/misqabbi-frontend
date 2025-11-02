515c80a01943bb6c5ca934ca62cad43eb2039db2
Anorme <anormeinkumsah@gmail.com>
2025-11-02 16:34:54 +0000

    refactor(Favorites): remove FavoritesProvider and related context, simplifying the application structure

--- src/contexts/favorites/favoritesContext.js
+++
@@ -1,5 +0,0 @@
-import { createContext } from 'react';
-
-export const FavoritesContextState = createContext();
-
-export const FavoritesContextDispatch = createContext();
--- src/contexts/favorites/favoritesSelectors.js
+++
@@ -1,9 +0,0 @@
-export const getFavoriteItems = state => Object.values(state.favoriteItems);
-
-export const isFavorite = (state, productId) => !!state.favoriteItems[productId];
-
-export const getFavoriteById = (state, productId) => state.favoriteItems[productId];
-
-export const getFavoritesLoading = state => state.isLoading;
-
-export const getFavoritesError = state => state.error;
