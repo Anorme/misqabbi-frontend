d5335fd7d84a9fdb014009784cac18b71acd43ae
Anorme <anormeinkumsah@gmail.com>
2025-11-04 03:19:13 +0000

    feat(NavBar): enhance responsive navigation by adding tablet support

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -2,11 +2,15 @@ import useMediaQuery from '../../hooks/useMediaQuery.js';
 
 import NavDesktop from './navigation/NavDesktop.jsx';
 import NavMobile from './navigation/NavMobile.jsx';
+import NavTablet from './navigation/NavTablet.jsx';
 
 const NavBar = () => {
-  const isMobile = useMediaQuery('(max-width: 768px)');
+  const isMobile = useMediaQuery('(max-width: 767px)');
+  const isTablet = useMediaQuery('(min-width: 768px) and (max-width: 1023px)');
 
-  return <div>{isMobile ? <NavMobile /> : <NavDesktop />}</div>;
+  if (isMobile) return <NavMobile />;
+  if (isTablet) return <NavTablet />;
+  return <NavDesktop />;
 };
 
 export default NavBar;
