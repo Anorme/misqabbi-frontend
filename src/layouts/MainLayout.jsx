5d04460a1454c67101b1ce01a7cfead7ccf77987
Anorme <anormeinkumsah@gmail.com>
2025-11-04 03:42:46 +0000

    fix(MainLayout): adjust padding for improved responsiveness on medium devices

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -5,7 +5,7 @@ const MainLayout = ({ children }) => {
   return (
     <div className="flex flex-col min-h-screen justify-between">
       <NavBar />
-      <div className="pt-18">{children}</div>
+      <div className="pt-18 md:pt-16">{children}</div>
       <Footer />
     </div>
   );
