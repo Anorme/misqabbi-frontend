07e4e433b81ae207be12345c48705b6abf4a4cea
Anorme <anormeinkumsah@gmail.com>
2025-11-04 03:17:52 +0000

    feat(CategoryNavigation): wire in new category navigation tablet component

--- src/components/layout/navigation/CategoryNavigation.jsx
+++ src/components/layout/navigation/CategoryNavigation.jsx
@@ -1,11 +1,15 @@
 import useMediaQuery from '../../../hooks/useMediaQuery.js';
 import CategoryNavigationDesktop from './CategoryNavigationDesktop.jsx';
 import CategoryNavigationMobile from './CategoryNavigationMobile.jsx';
+import CategoryNavigationTablet from './CategoryNavigationTablet.jsx';
 
 const CategoryNavigation = () => {
-  const isMobile = useMediaQuery('(max-width: 768px)');
+  const isMobile = useMediaQuery('(max-width: 767px)');
+  const isTablet = useMediaQuery('(min-width: 768px) and (max-width: 1023px)');
 
-  return isMobile ? <CategoryNavigationMobile /> : <CategoryNavigationDesktop />;
+  if (isMobile) return <CategoryNavigationMobile />;
+  if (isTablet) return <CategoryNavigationTablet />;
+  return <CategoryNavigationDesktop />;
 };
 
 export default CategoryNavigation;
