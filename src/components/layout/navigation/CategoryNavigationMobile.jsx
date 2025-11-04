c896765e4e3e802cbd3f8749f5a1fcf18caeb693
Anorme <anormeinkumsah@gmail.com>
2025-11-04 02:31:06 +0000

    refactor(CategoryNavigationMobile): adjust top position and z-index

--- src/components/layout/navigation/CategoryNavigationMobile.jsx
+++ src/components/layout/navigation/CategoryNavigationMobile.jsx
@@ -17,7 +17,7 @@ const CategoryNavigationMobile = () => {
   };
 
   return (
-    <div className="fixed top-[72px] left-0 right-0 z-40 w-full border-t border-b border-[#949396] bg-white">
+    <div className="fixed top-[64px] left-0 right-0 z-30 w-full border-t border-b border-[#949396] bg-white">
       <nav className="w-full" aria-label="Category Navigation">
         <div className="max-w-7xl mx-auto px-2 sm:px-4">
           <div className="flex items-center justify-between h-12">
