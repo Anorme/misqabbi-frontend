41675dd1fb664083f767e4179973c4d5c8f348ba
Anorme <anormeinkumsah@gmail.com>
2025-11-05 16:31:14 +0000

    fix(ProductInfo): update heading color and adjust detail heading size for improved consistency

--- src/components/products/ProductInfo.jsx
+++ src/components/products/ProductInfo.jsx
@@ -1,13 +1,13 @@
 function ProductInfo({ product }) {
   return (
     <div className="flex flex-col gap-2 pb-4 sm:pb-6">
-      <h1 className="font-extrabold text-lg sm:text-xl md:text-2xl tracking-tight text-[#7D3C98]">
+      <h1 className="font-extrabold text-lg sm:text-xl md:text-2xl tracking-tight text-msq-purple">
         {product?.name}
       </h1>
       <p className="text-xl sm:text-2xl font-extrabold text-msq-purple-deep hidden lg:block">
         GHC {product?.price}
       </p>
-      <h2 className="text-lg sm:text-xl md:text-2xl">Details</h2>
+      <h2 className="text-lg sm:text-xl lg:text-2xl">Details</h2>
       <p className="text-sm sm:text-base">{product?.description}</p>
     </div>
   );
