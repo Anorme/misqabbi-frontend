e8ad4471e2bb8c69f561601f89aeeca80187239c
Anorme <anormeinkumsah@gmail.com>
2025-11-13 13:38:41 +0000

    feat(TrendingItemCard): add link functionality to trending item card component

--- src/components/landingpage/TrendingItemCard.jsx
+++ src/components/landingpage/TrendingItemCard.jsx
@@ -1,16 +1,20 @@
-function TrendingItemCard({ name, image, price }) {
+import { Link } from 'react-router';
+
+function TrendingItemCard({ name, image, price, url }) {
   return (
     <div className="border-none">
       <div className="aspect-[3/4] w-full overflow-hidden">
-        <img
-          src={image}
-          alt={name}
-          className="w-full h-full object-cover rounded-none"
-          loading="lazy"
-          decoding="async"
-          width={600}
-          height={800}
-        />
+        <Link to={url}>
+          <img
+            src={image}
+            alt={name}
+            className="w-full h-full object-cover rounded-none"
+            loading="lazy"
+            decoding="async"
+            width={600}
+            height={800}
+          />
+        </Link>
       </div>
       <div>
         <h3 className="mt-3 text-[14px] md:text-[16px] lg:text-[18px] text-msq-purple font-lato text-left text-nowrap ">
